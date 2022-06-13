463c8855af8c27efbdcf88cbcd1f7488789fc53b
Muhammad-Talha028989 <muhammadtalha028989@gmail.com>
2022-06-13 14:45:32 +0500

    Added formSection like profile and etc...

--- View/src/Components/Editor/Editor.jsx
+++ View/src/Components/Editor/Editor.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Summery from "./conponents.editor/FormSection/Summery";
 import DownloadButton from "./conponents.editor/Stepper/Button/DownloadButton";
 import Stepper from "./conponents.editor/Stepper/Stepper";
 import StepperControl from "./conponents.editor/Stepper/StepperControl";
@@ -7,18 +8,39 @@ export class Editor extends Component {
   render() {
     return (
       <>
-        <div className="absolute top-0 bottom-0 md-max-width:w-full w-1/2 mx-auto shadow-xl rounded-xl pb-2 bg-white">
-          <div className="absolute top-0 right-0 h-16 w-full bg-gray-100 flex justify-end py-3 pr-6">
+        <div className="absolute top-0 bottom-0 lg-max-width:w-full w-1/2 mx-auto shadow-xl rounded-xl pb-2 bg-white">
+          <div className="absolute top-0 right-0 h-16 w-full rounded-md shadow-lg z-10 bg-gray-100 flex justify-end py-3 pr-6">
             <DownloadButton />
             <i className="bx bx-md bxs-book-content absolute left-0 ml-4 cursor-pointer hover:text-red-300"></i>
             <i class="bx bx-md bxl-sketch absolute left-12 ml-4 cursor-pointer hover:text-red-300"></i>
           </div>
+          {/* profile form */}
+          <Summery/>
+          {/* personal form */}
 
-          {/* <div className="container horizontal mt-5">
-            <Stepper />
-          </div>
+          {/* education form */}
 
-          <StepperControl /> */}
+          {/* skill form */}
+
+          {/* certificate form */}
+
+          {/* award form */}
+
+          {/* experience form */}
+
+          {/* language form */}
+
+          {/* interest form */}
+
+          {/* course form */}
+
+          {/* organization form */}
+
+          {/* reference form */}
+
+          {/* projects form */}
+
+          {/* publication form */}
         </div>
       </>
     );
---
+++ View/src/Components/Editor/conponents.editor/FormSection/Summery.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Summery = () => {
+  return (
+    <div>
+      <div className=" relative top-16 mt-4">
+        <details className=" relative bg-white w-full shadow rounded group">
+          <summary className="list-none flex flex-wrap items-center cursor-pointer w-full  lg:w-full">
+            <h3 className="flex flex-1 p-4 font-bold uppercase text-red-300">
+              Profile
+            </h3>
+            <div className="flex w-10 items-center justify-center">
+              <div className="border-8 border-transparent border-l border-l-gray-600 ml-2 group-open:rotate-90 transition-transform origin-left">
+                <i className="bx bxs-right-arrow bx-flip-vertical group-open:text-red-400"></i>
+              </div>
+            </div>
+          </summary>
+          <div className="p-4">{/* form here */}</div>
+        </details>
+      </div>
+    </div>
+  );
+};
+
+export default Summery;
